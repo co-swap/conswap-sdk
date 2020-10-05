494ca48b67a2a4eaf8407f1266ac84b102d9a6df
rowen <xubowen@rowen.work>
2020-10-05 15:10:52 +0800

    update address

--- src/entities/token.ts
+++ src/entities/token.ts
@@ -59,25 +59,25 @@ export function currencyEquals(currencyA: Currency, currencyB: Currency): boolea
 export const WETH = {
   [ChainId.MAINNET]: new Token(
     ChainId.MAINNET,
-    '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2',
+    '0x8b0042e6274024443f6ee519204da7a951ac6244',
     18,
     'WETH',
     'Wrapped Ether'
   ),
   [ChainId.ROPSTEN]: new Token(
     ChainId.ROPSTEN,
-    '0xc778417E063141139Fce010982780140Aa0cD5Ab',
+    '0x8b0042e6274024443f6ee519204da7a951ac6244',
     18,
     'WETH',
     'Wrapped Ether'
   ),
   [ChainId.RINKEBY]: new Token(
     ChainId.RINKEBY,
-    '0xc778417E063141139Fce010982780140Aa0cD5Ab',
+    '0x8b0042e6274024443f6ee519204da7a951ac6244',
     18,
     'WETH',
     'Wrapped Ether'
   ),
-  [ChainId.GÖRLI]: new Token(ChainId.GÖRLI, '0xB4FBF271143F4FBf7B91A5ded31805e42b2208d6', 18, 'WETH', 'Wrapped Ether'),
-  [ChainId.KOVAN]: new Token(ChainId.KOVAN, '0xd0A1E359811322d97991E03f863a0C30C2cF029C', 18, 'WETH', 'Wrapped Ether')
+  [ChainId.GÖRLI]: new Token(ChainId.GÖRLI, '0x8b0042e6274024443f6ee519204da7a951ac6244', 18, 'WETH', 'Wrapped Ether'),
+  [ChainId.KOVAN]: new Token(ChainId.KOVAN, '0x8b0042e6274024443f6ee519204da7a951ac6244', 18, 'WETH', 'Wrapped Ether')
 }
